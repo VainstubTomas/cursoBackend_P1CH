b99f14e9d2d2e3cb125bacd4f6dbfb1ccc54dff2
Tomas <vainstubtomas@gmail.com>
2025-11-05 21:59:56 -0300

    feature: finalizada funcionalidades entrega 2

---
+++ entregas/src/utils/uploader.js
@@ -0,0 +1,16 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "./public/images");
+    },
+    filename: (req, file, callback) => {
+        const newFileName = Date.now()+"-"+file.originalname;
+        callback(null, newFileName);
+    }
+});
+
+//middleware - intermediario entre solicitudes
+const uploader = multer({storage});
+
+export default uploader;
